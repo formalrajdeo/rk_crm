f98b80f9732617078fae9a1d6d87950053a36eb9
raj <rajkamalprasad512@gmail.com>
2023-06-24 00:00:26 +0530

    code cleanup

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,5 +1,2 @@
 export { default as LOGO } from './logo.png'
-export { default as BASIC_INFO } from './basic-info.png'
-export { default as CLIENT_ACCESS } from './client-access.png'
-export { default as FIRM_ACCESS } from './firm-access.png'
 export { default as MODAL_BG_IMAGE } from './modal-bg.png'
--- assets/svgs/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as GreaterThan } from './greater-then.svg'
-export { default as BasicInfo } from './basic-info.svg'
-export { default as ClientAccess } from './client-access.svg'
-export { default as FirmAccess } from './firm-access.svg'
